11c44407dd5c0cdfd07bd6519833a0743d8e6db4
LP11 <LPinero@lisa-pineros-macbook.local>
2016-03-18 21:18:49 -0400

    using a jQuery plug in on my final project

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// EXAMPLE TAGS
+
+// $('#grayButton').click(switchGray);
+// $('#whiteButton').click(switchWhite);
+
+
+// function switchGray() {
+  // $('body').css('backgroundColor', 'gray');
+// }
+// function switchWhite() {
+  // $('body').css('backgroundColor', 'white');
+// };
+
+// Enable Carousel Controls
+// $(".left").click(function(){
+//     $("#myCarousel").carousel("prev");
+// });
+
+// $(".right").click(function(){
+// 	$("myCarousel").carousel("next");
+// // }
+// });
